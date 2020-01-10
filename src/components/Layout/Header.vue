bc1da22782044252ebdfd1b51a46a90fcc266f20
romandev314 <romandev314@gmail.com>
2020-01-10 20:03:56 +0200

    start

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>header</div>
+	<header class="header">
+		<div class="header__logo-wrap">
+			<a class="header__logo" href="#top-anchor">
+				<div class="header__logo-brand" hidden>
+					<abbr class="header__logo-text  header__logo-text--1" title="HyperText Markup Language">
+						html
+					</abbr>
+
+					<div class="header__logo-pic">
+						<div class="icon-css"></div>
+					</div>
+				</div>
+
+				<div class="header__logo-brand">
+					<abbr class="header__logo-text  header__logo-text--2" title="Cascading Style Sheets">
+						vue js
+					</abbr>
+
+					<div class="header__logo-pic">
+						<div class="icon-vue"></div>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="header__menu">
+			<Menu></Menu>
+		</div>
+	</header>
 </template>
+
+<script>
+	import Menu from './Menu'
+
+	export default {
+		components: {
+			Menu: Menu,
+		},
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
---
+++ src/components/Layout/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+	<nav class="menu">
+		<div class="menu__open-wrap">
+			<button class="menu__open-btn" type="button">
+				<span class="menu__open-line"></span>
+				<span class="menu__open-line"></span>
+				<span class="menu__open-line"></span>
+			</button>
+		</div>
+
+		<div class="menu__desc">
+			<div class="drop-menu">
+				<div class="menu-desc">
+					<div class="menu-desc__avatar">
+						<div class="user">
+
+							<img class="menu__avatar"
+								 src="../../assets/images/pic/roman-yurchenko.jpg"
+								 alt="Roman Yurchenko">
+
+							<div class="name">
+								Roman Yurchenko
+							</div>
+						</div>
+					</div>
+
+					<div class="menu-desc__nav">
+						<nav class="nav-main">
+							<ul class="nav-main__list">
+								<li class="nav-main__item">
+									<router-link class="nav-main__link" to="/">Мои работы</router-link>
+								</li>
+
+								<li class="nav-main__item">
+									<router-link class="nav-main__link" to="/about">Обо мне</router-link>
+								</li>
+
+								<li class="nav-main__item">
+									<router-link class="nav-main__link" to="/contacts">Контакты</router-link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
