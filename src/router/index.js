811f52f3341d36edb4388ec99ce22333e5837e89
romandev314 <romandev314@gmail.com>
2020-01-14 23:28:33 +0200

    add about

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
 	{
 		path: '/about',
 		name: 'about',
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+		component: () => import(/* webpackChunkName: "about" */ '../components/About/About.vue')
 	},
 	{
 		path: '/contacts',
